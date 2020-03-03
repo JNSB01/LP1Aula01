a5933d662f1abb143d3780b284cb8a49df858b33
João Borges <joaonunos.borges@gmail.com>
2020-03-03 21:04:35 +0000

    bools

--- TiposVarios/Program.cs
+++ TiposVarios/Program.cs
@@ -15,10 +15,13 @@ namespace TiposVarios
             decimal d = 1.1245m;
             float f = 2.43f;
             double dec = 5.0e-10;
+            bool t = true;
+            bool fal = 3<1;
 
             Console.WriteLine($"{i}, {r}, {y}, {h}");
             Console.WriteLine($"{cps}, {n}{cps}");
             Console.WriteLine($"{d}, {f}, {dec}");
+            Console.WriteLine($"{t}, {fal}");
         }
     }
 }
