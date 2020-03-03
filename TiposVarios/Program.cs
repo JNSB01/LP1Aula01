5668c26102b18db9df6f9bdf8e30837844bb8e4e
João Borges <joaonunos.borges@gmail.com>
2020-03-03 20:54:24 +0000

    chars

--- TiposVarios/Program.cs
+++ TiposVarios/Program.cs
@@ -10,8 +10,11 @@ namespace TiposVarios
             uint r = 4U;
             byte y = 6;
             long h = 0x10;
+            char cps = '\u00A9';
+            char n = '\x000A';
 
             Console.WriteLine($"{i}, {r}, {y}, {h}");
+            Console.WriteLine($"{cps}, {n}{cps}");
         }
     }
 }
