c6ef9d0fb1eed226ceafab2ce44806095d4cfa12
João Borges <joaonunos.borges@gmail.com>
2020-03-03 20:49:23 +0000

    Inteiros

---
+++ TiposVarios/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace TiposVarios
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 3;
+            uint r = 4U;
+            byte y = 6;
+            long h = 0x10;
+
+            Console.WriteLine($"{i}, {r}, {y}, {h}");
+        }
+    }
+}
