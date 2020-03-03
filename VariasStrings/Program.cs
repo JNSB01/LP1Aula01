e2a82008a088d3d29339b8faa1a5d13abbe3fa55
João Borges <joaonunos.borges@gmail.com>
2020-03-03 23:06:08 +0000

    strings

---
+++ VariasStrings/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace VariasStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "Hello world";
+            string v2 = "Nova linha \n Unicode e \u1234";
+            string v3 = @"Nova linha \n Unicode e \u1234";
+            string v4 = "\"String entre \t aspas\"";
+            string v5 = @"""String entre aspas""";
+            Console.WriteLine(s);
+            Console.WriteLine(v2);
+            Console.WriteLine(v3);
+            Console.WriteLine(v4);
+            Console.WriteLine(v5);
+        }
+    }
+}
